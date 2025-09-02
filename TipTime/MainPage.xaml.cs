2cc1b93b5e6eb0f60d91fe5c583ed36b27c3f433
menezess-duda <mariaeduardamenezes0706@gmail.com>
2025-09-01 22:29:07 -0300

    Aula 03 Slider's

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+       // float = 
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+     
+        }
+
+       
+
+        private void ArredondaCimaBtn_Clicked(object sender, EventArgs e)
+        {
+            count++;
+
+        }
+
+        private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+        }
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+        }
+
+        private void ArredondaBaixoBtn_Clicked(object sender, EventArgs e)
+        {
+            //PorcentagemGorjetaLabel = ValorTotalEntry; 
+            
+        }
+
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // ValorGorjetaLabel = ValorFinalLabel * (PorcentagemGorjetaLabel / 100);
+
+           // ValorTotalEntry = float;
+
+
+        }
+    }
+
+}
